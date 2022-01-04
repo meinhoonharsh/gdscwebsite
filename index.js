32e4aed1219d15a668e7276f02b1fe756d5fd889
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-04 23:51:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
+
+// use public folder for static files
+app.use(express.static(path.join(__dirname, 'public')));
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "Hey", message: "Hello there!" });
+});
+
+app.use("/", router);
+app.listen(process.env.port || 8000);
+
+console.log("Running at Port 3000");
