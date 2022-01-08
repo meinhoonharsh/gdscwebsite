4e7dcfae0b83cdd1e8df66a49b98a229da639b6e
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-01-09 02:01:36 +0530

    Added axios call and other featres

--- index.js
+++ index.js
@@ -4,19 +4,35 @@ const path = require("path");
 const departments = require('./json/departments.json');
 const partners = require('./json/communitypartners.json');
 const teams = require('./json/teams.json');
+const axios = require('axios')
+const { resolve } = require("path");
+const { param } = require("express/lib/request");
+
 const router = express.Router();
 app.set('views', path.join(__dirname, 'templates'));
 app.set('view engine', 'ejs');
+
+ 
 
 
 // use public folder for static files
 app.use(express.static(path.join(__dirname, 'public')));
 router.get("/", (req, res) => {
-  res.render("index",{
+  
+  // const teamsData = axios.get('http://localhost:1337/api/teams')
+  axios.get('http://localhost:1337/api/teams')
+  .then(response => {
+
+  const params = {
     departments: departments,
     partners: partners,
-    teams: teams
-  });
+    teams: response.data.data
+  }
+  // console.log(params)
+    res.render("index", params);
+  })
+
+
 });
 
 router.get("/contact", (req, res) => {
@@ -25,6 +41,11 @@ router.get("/contact", (req, res) => {
     partners: partners,
     teams: teams
   });
+});
+
+router.get("/axios", (req, res) => {
+  // Api call to localhost:1337/api/teams using http
+ res.send("I am working Fine")
 });
 
 router.get("/about", (req, res) => {
